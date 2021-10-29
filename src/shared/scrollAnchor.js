5c562723bee14a3d533e1aa3feedbe9b5599adf8
zcw <2352427469@qq.com>
2021-10-29 14:55:53 +0800

    bugfix : margin没有被缩放，计算偏移量补偿

--- src/shared/scrollAnchor.js
+++ src/shared/scrollAnchor.js
@@ -1,9 +1,14 @@
-// const docPageMarginTop=20;
+const docPageMarginTop = 20;
 
-const scrollAnchor=(node,cur,nxt)=>{
+const scrollAnchor = (node, cur, nxt) => {
     console.log(node.scrollTop)
-    console.log(cur,nxt)
-    node.scrollTop = node.scrollTop * nxt.height/cur.height
-    console.log(node.scrollTop * nxt.height/cur.height)
+    console.log(cur, nxt)
+    let curPage = Math.floor(node.scrollTop / cur.height);
+    console.log("当前页数为：" + curPage)
+    // 由于margin没有被缩放所以需要计算偏移量补偿
+    let offsetCompensation = (1-nxt.height/cur.height)*curPage*docPageMarginTop;
+    console.log("偏移量补偿为："+offsetCompensation)
+    node.scrollTop = node.scrollTop * nxt.height / cur.height + offsetCompensation
+    console.log(node.scrollTop)
 }
 export default scrollAnchor;
